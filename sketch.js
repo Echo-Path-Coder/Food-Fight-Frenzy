e82c839f67787b477c69e27c4447268194f842ab
Echo-Path-Coder <Echopath2023@outlook.com>
2025-10-20 22:53:13 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -104,10 +104,6 @@ function preload() {
 
 function setup() {
   gameCanvas = createCanvas(700, 500);
-
-// Prevent browser default touch actions (like scrolling)
-  document.body.style.touchAction = "none";
-  document.documentElement.style.overflow = "hidden";
   
   gui = createGui();
 
@@ -180,13 +176,13 @@ playButton2 = createButton("Play", width / 2 - 50, height / 2 + 25, 150, 75);
   initialsInput = createInput();
   initialsInput.attribute("maxlength", 30);
   initialsInput.attribute("placeholder", "Enter Initials");
-  initialsInput.size(100, 25);
+  initialsInput.size(100);
   initialsInput.input(saveInput);
 
   locationSelect = createInput();
   locationSelect.attribute("maxlength", 30);
   locationSelect.attribute("placeholder", "Enter Location");
-  locationSelect.size(150, 25);
+  locationSelect.size(150);
   locationSelect.input(saveInput);
 
   positionInputs(); // fix input alignment
@@ -350,30 +346,4 @@ function mouseClicked() {
 function saveInput(initials, location) {
   localStorage.setItem("playerInitials", initials);
   localStorage.setItem("playerLocation", location);
-}
-
-function touchStarted() {
-  // Check if the touch is within the bounds of the input box
-  if (
-    mouseX > initialsInput.x &&
-    mouseX < initialsInput.x + initialsInput.width &&
-    mouseY > initialsInput.y &&
-    mouseY < initialsInput.y + initialsInput.height
-  ) {
-    // Focus the input element to bring up the keyboard
-    initialsInput.elt.focus();
-  }
-  
-  // Check if the touch is within the bounds of the input box
-  if (
-    mouseX > locationSelect.x &&
-    mouseX < locationSelect.x + locationSelect.width &&
-    mouseY > locationSelect.y &&
-    mouseY < locationSelect.y + locationSelect.height
-  ) {
-    // Focus the input element to bring up the keyboard
-    locationSelect.elt.focus();
-  }
-  // This is important to prevent default browser behavior
-  return false;
 }
